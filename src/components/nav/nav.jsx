c276bed01ed3ff5443e3af99738349b513629f37
Carlos <108499943+carlos1-1@users.noreply.github.com>
2023-01-02 06:26:13 -0300

    responsive v1

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,92 +4,103 @@ import {
   AiFillGithub,
   AiFillLinkedin,
   AiOutlineMail,
+  AiOutlineMenuUnfold,
 } from "react-icons/ai";
 import { SiAboutdotme } from "react-icons/si";
 import { DiJavascript } from "react-icons/di";
 import { FaProjectDiagram } from "react-icons/fa";
+import React from "react";
 
 export default function Nav() {
-  return (
-    <div className="sidebar">
-      <ul className="nav-list">
-        <li>
-          <a href="/">
-            <i className="bx bx-grid-alt">
-              <AiOutlineHome />
-            </i>
+  const toggleMenu = () => {
+    let navigation = document.querySelector(".navigation");
+    let toggle = document.querySelector(".toggle");
+    navigation.classList.toggle("active");
+    toggle.classList.toggle("active");
+  };
 
-            <span className="links_name">Inicio</span>
-          </a>
-          <span className="tooltip">Inicio</span>
-        </li>
-        <li>
-          <a href="/AboutMe">
-            <i className="bx bx-user">
-              <SiAboutdotme />
-            </i>
-            <span className="links_name">Sobre mi</span>
-          </a>
-          <span className="tooltip">Sobre mi</span>
-        </li>
-        <li>
-          <a href="Skills">
-            <i className="bx bx-chat">
-              <DiJavascript />
-            </i>
-            <span className="links_name">Habilidades</span>
-          </a>
-          <span className="tooltip">Habilidades</span>
-        </li>
-        <li>
-          <a href="Proyects">
-            <i className="bx bx-pie-chart-alt-2">
-              <FaProjectDiagram />
-            </i>
-            <span className="links_name">Proyectos</span>
-          </a>
-          <span className="tooltip">Proyectos</span>
-        </li>
-        <li>
-          <a
-            href="https://github.com/carlos1-1"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bx-folder">
-              <AiFillGithub />
-            </i>
-            <span className="links_name">Github</span>
-          </a>
-          <span className="tooltip">Github</span>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/carlos-g%C3%B3mez-057a0a24a/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bx-cart-alt">
-              <AiFillLinkedin />
-            </i>
-            <span className="links_name">LinkedIn</span>
-          </a>
-          <span className="tooltip">LinkedIn</span>
-        </li>
-        <li>
-          <a
-            href="mailto:carlosfrangomez@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bx-heart">
-              <AiOutlineMail />
-            </i>
-            <span className="links_name">Correo</span>
-          </a>
-          <span className="tooltip">Correo</span>
-        </li>
-      </ul>
+  return (
+    <div>
+      <div className="navigation">
+        <ul>
+          <li>
+            <a href="/">
+              <span className="icon">
+                <AiOutlineHome />
+                <i className="fa fa-home" aria-hidden="true"></i>
+              </span>
+              <span className="title">Inicio</span>
+            </a>
+          </li>
+          <li>
+            <a href="/AboutMe">
+              <span className="icon">
+                <SiAboutdotme />
+                <i className="fa fa-user" aria-hidden="true"></i>
+              </span>
+              <span className="title">Sobre mi</span>
+            </a>
+          </li>
+          <li>
+            <a href="/Skills">
+              <span className="icon">
+                <DiJavascript />
+                <i className="fa fa-comment" aria-hidden="true"></i>
+              </span>
+              <span className="title">Habilidades</span>
+            </a>
+          </li>
+          <li>
+            <a href="Proyects">
+              <span className="icon">
+                <FaProjectDiagram />
+                <i className="fa fa-question-circle" aria-hidden="true"></i>
+              </span>
+              <span className="title">Proyectos</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/carlos1-1"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="icon">
+                <AiFillGithub />
+                <i className="fa fa-cog" aria-hidden="true"></i>
+              </span>
+              <span className="title">Github</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/carlos-g%C3%B3mez-057a0a24a/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="icon">
+                <AiFillLinkedin />
+                <i className="fa fa-lock" aria-hidden="true"></i>
+              </span>
+              <span className="title">Linkedin</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:carlosfrangomez@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="icon">
+                <AiOutlineMail />
+                <i className="fa fa-sign-out" aria-hidden="true"></i>
+              </span>
+              <span className="title">Correo</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <AiOutlineMenuUnfold className="toggle" onClick={toggleMenu} />
     </div>
   );
 }
